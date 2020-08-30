cdcdbe463e4c203fed8435562e9e902a55755586
pauvalenzuela <paulreggieevalenzuela@gmail.com>
2020-08-30 23:31:34 +0800

    Updates

---
+++ precache-manifest.ab1a73153b91cb8e8a818646d69d2b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bbb52ccd36daf85bf14eee721b7050",
+    "url": "/crazy-typing/index.html"
+  },
+  {
+    "revision": "f6f512fd430abb9a2418",
+    "url": "/crazy-typing/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "f8c2d0e5c563a7be88af",
+    "url": "/crazy-typing/static/css/main.ce053b81.chunk.css"
+  },
+  {
+    "revision": "f6f512fd430abb9a2418",
+    "url": "/crazy-typing/static/js/2.923d7c56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crazy-typing/static/js/2.923d7c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c2d0e5c563a7be88af",
+    "url": "/crazy-typing/static/js/main.5673158d.chunk.js"
+  },
+  {
+    "revision": "f3e5b193d9d0d7865796",
+    "url": "/crazy-typing/static/js/runtime-main.86b1f1c3.js"
+  }
+]);
